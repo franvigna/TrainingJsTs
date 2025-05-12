7d601bbd1cf262d868a4d79012a4ac4923e6069e
Francisco Vignardel <fvignardel@circostudio.com>
2025-05-12 10:28:26 -0300

    base de la pagina

---
+++ JS/1. Fundamentos de JavaScript/script.js
@@ -0,0 +1,4 @@
+function mostrarMensaje() {
+    const mensaje = '¡Hola! Este es tu primer script en JavaScript.'
+    document.getElementById('resultado').innerText = mensaje
+}
---
+++ JS/3. Manejo de Funciones Avanzadas/script.js
@@ -0,0 +1,4 @@
+function ejecutar() {
+  // Código de ejemplo para mostrar resultados
+  document.getElementById("resultado").innerText = "Resultado de la función: Hola desde JavaScript!";
+}
