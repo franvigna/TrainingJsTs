d04def0edadd9b3841275384f7c4d2e54e801ae8
Cisco <69130363+franvigna@users.noreply.github.com>
2025-05-13 10:03:24 -0300

    2.manejo de funciones avanzadas

--- JS/3. Manejo de Funciones Avanzadas/script.js
+++ JS/3. Manejo de Funciones Avanzadas/script.js
@@ -28,4 +28,60 @@ const devs = personas.filter((p) => p.profesion === 'Desarrollo Web')
 console.log('Personas de desarrollo web: ', devs)
 
 //reduce
-const sumaEdades = personas.reduce(())
+const sumaEdades = personas.reduce((acc, p) => acc + p.edad, 0)
+console.log('Suma total de edades: ', sumaEdades)
+
+// ===================================
+//2.Funciones que devuelven funciones
+// ===================================
+
+function crearSaludo(saludo) {
+    return function (nombre) {
+        return `${saludo}, ${nombre}!`
+    }
+}
+
+const saludoHola = crearSaludo('Hola')
+console.log('Saludo personalizado: ', saludoHola('Cisco'))
+
+// ===================================
+//3. Callbacks
+// ===================================
+
+function procesarPersona(persona, callback) {
+    callback(persona)
+}
+
+procesarPersona(personas[1], function (p) {
+    console.log('Procesando persona con callback', p.nombre)
+})
+
+// ===================================
+// 4. Promesas
+// ===================================
+
+function obtenerDolar() {
+    return fetch('https://dolarapi.com/v1/dolares/oficial')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Error HTTP ${response.status}`)
+            }
+            return response.json()
+        })
+
+        .then((data) => {
+            console.log('Cotizacion dolar oficial: ')
+            console.log(`Compra: ${data.compra}`)
+            console.log(`Venta: ${data.venta}`)
+            console.log(`Frecha Actualizado: ${data.fechaActualizacion}`)
+        })
+
+        .catch((error) => {
+            console.error('No se pudo obtener la cotizacion: ', error.message)
+        })
+
+        .finally(() => {
+            console.log('Consulta de tipo de cambio completada')
+        })
+}
+obtenerDolar()
