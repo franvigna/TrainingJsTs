13bcd43cca6a5c1d042b97bb5ba15c465477e6f8
Cisco <69130363+franvigna@users.noreply.github.com>
2025-05-13 09:30:04 -0300

    3. Manejo de funciones avanzadas

--- JS/3. Manejo de Funciones Avanzadas/script.js
+++ JS/3. Manejo de Funciones Avanzadas/script.js
@@ -1,4 +1,31 @@
 function ejecutar() {
-  // Código de ejemplo para mostrar resultados
-  document.getElementById("resultado").innerText = "Resultado de la función: Hola desde JavaScript!";
+    // Código de ejemplo para mostrar resultados
+    document.getElementById('resultado').innerText =
+        'Resultado de la función: Hola desde JavaScript!'
 }
+
+const personas = [
+    { nombre: 'Tomi', edad: 25, profesion: 'Ingenieria Informatica' },
+    { nombre: 'Cisco', edad: 21, profesion: 'Ingenieria Informatica' },
+    { nombre: 'Mili', edad: 23, profesion: 'Desarrollo Web' },
+    { nombre: 'Leandro', edad: 21, profesion: 'Desarrollo Web' },
+    { nombre: 'Martin', edad: 24, profesion: 'Ingenieria Informatica' },
+    { nombre: 'Gonzalo', edad: 24, profesion: 'Ingenieria Informatica' },
+    { nombre: 'Yami', edad: 23, profesion: 'Desarrollo Web' },
+    { nombre: 'Rocio', edad: 23, profesion: 'Ingenieria Informatica' },
+]
+
+// ===================================
+//1. Funciones de orden superior
+// ===================================
+
+//map
+const nombresMayus = personas.map((p) => p.nombre.toUpperCase())
+console.log('NombresEn mayúscula: ', nombresMayus)
+
+//filter
+const devs = personas.filter((p) => p.profesion === 'Desarrollo Web')
+console.log('Personas de desarrollo web: ', devs)
+
+//reduce
+const sumaEdades = personas.reduce(())
